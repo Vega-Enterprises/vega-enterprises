4b980860fa87b439d679275b4bfbb873461e8a6a
Vega-Enterprises <148875199+Vega-Enterprises@users.noreply.github.com>
2023-10-24 18:51:34 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,184 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 10000); // Change image every 2 seconds
+}
+
+
+function toggleMenu() {
+  var menu = document.getElementById("menu");
+  if (menu.style.display === "block") {
+      menu.style.display = "none";
+  } else {
+      menu.style.display = "block";
+  }
+}
+
+document.getElementById("click1").addEventListener("click", function() {
+  // Redirect to so.html
+  window.location.href = "so.html";
+});
+
+document.getElementById("click2").addEventListener("click", function() {
+  // Redirect to so.html
+  window.location.href = "so.html";
+});
+document.getElementById("click3").addEventListener("click", function() {
+  // Redirect to so.html
+  window.location.href = "so.html";
+});
+document.getElementById("click4").addEventListener("click", function() {
+  // Redirect to so.html
+  window.location.href = "so.html";
+});
+document.getElementById("click5").addEventListener("click", function() {
+  // Redirect to so.html
+  window.location.href = "so.html";
+});
+document.getElementById("click6").addEventListener("click", function() {
+  // Redirect to so.html
+  window.location.href = "so.html";
+});
+document.getElementById("click7").addEventListener("click", function() {
+  // Redirect to so.html
+  window.location.href = "so.html";
+});
+document.getElementById("click8").addEventListener("click", function() {
+  // Redirect to so.html
+  window.location.href = "so.html";
+});
+document.getElementById("click9").addEventListener("click", function() {
+  // Redirect to so.html
+  window.location.href = "so.html";
+});
+document.getElementById("click10").addEventListener("click", function() {
+  // Redirect to so.html
+  window.location.href = "so.html";
+});
+document.getElementById("click11").addEventListener("click", function() {
+  // Redirect to so.html
+  window.location.href = "so.html";
+});
+document.getElementById("click12").addEventListener("click", function() {
+  // Redirect to so.html
+  window.location.href = "so.html";
+});
+document.getElementById("click13").addEventListener("click", function() {
+  // Redirect to so.html
+  window.location.href = "so.html";
+});
+document.getElementById("click14").addEventListener("click", function() {
+  // Redirect to so.html
+  window.location.href = "so.html";
+});
+document.getElementById("click15").addEventListener("click", function() {
+  // Redirect to so.html
+  window.location.href = "so.html";
+});
+document.getElementById("click16").addEventListener("click", function() {
+  // Redirect to so.html
+  window.location.href = "so.html";
+});
+document.getElementById("click17").addEventListener("click", function() {
+  // Redirect to so.html
+  window.location.href = "so.html";
+});
+document.getElementById("click18").addEventListener("click", function() {
+  // Redirect to so.html
+  window.location.href = "so.html";
+});
+document.getElementById("click19").addEventListener("click", function() {
+  // Redirect to so.html
+  window.location.href = "so.html";
+});
+document.getElementById("click20").addEventListener("click", function() {
+  // Redirect to so.html
+  window.location.href = "so.html";
+});
+document.getElementById("click21").addEventListener("click", function() {
+  // Redirect to so.html
+  window.location.href = "so.html";
+});
+document.getElementById("click22").addEventListener("click", function() {
+  // Redirect to so.html
+  window.location.href = "so.html";
+});
+document.getElementById("click23").addEventListener("click", function() {
+  // Redirect to so.html
+  window.location.href = "so.html";
+});
+document.getElementById("click24").addEventListener("click", function() {
+  // Redirect to so.html
+  window.location.href = "so.html";
+});
+document.getElementById("click25").addEventListener("click", function() {
+  // Redirect to so.html
+  window.location.href = "so.html";
+});
+document.getElementById("click26").addEventListener("click", function() {
+  // Redirect to so.html
+  window.location.href = "so.html";
+});
+document.getElementById("click27").addEventListener("click", function() {
+  // Redirect to so.html
+  window.location.href = "so.html";
+});
+document.getElementById("click28").addEventListener("click", function() {
+  // Redirect to so.html
+  window.location.href = "so.html";
+});
+document.getElementById("click29").addEventListener("click", function() {
+  // Redirect to so.html
+  window.location.href = "so.html";
+});
+document.getElementById("click30").addEventListener("click", function() {
+  // Redirect to so.html
+  window.location.href = "so.html";
+});
+document.getElementById("click31").addEventListener("click", function() {
+  // Redirect to so.html
+  window.location.href = "so.html";
+});
+document.getElementById("click32").addEventListener("click", function() {
+  // Redirect to so.html
+  window.location.href = "so.html";
+});
+document.getElementById("click33").addEventListener("click", function() {
+  // Redirect to so.html
+  window.location.href = "so.html";
+});
+document.getElementById("click34").addEventListener("click", function() {
+  // Redirect to so.html
+  window.location.href = "so.html";
+});
+document.getElementById("click35").addEventListener("click", function() {
+  // Redirect to so.html
+  window.location.href = "so.html";
+});
+document.getElementById("click36").addEventListener("click", function() {
+  // Redirect to so.html
+  window.location.href = "so.html";
+});
+document.getElementById("click37").addEventListener("click", function() {
+  // Redirect to so.html
+  window.location.href = "so.html";
+});
+document.getElementById("click38").addEventListener("click", function() {
+  // Redirect to so.html
+  window.location.href = "so.html";
+});
+
